1d77f0b85a2c95c1a289f4c4b7e247995349e5e1
Armand Welsh <armand@welshhome.org>
2019-03-17 15:05:00 -0700

    Update roku-app-child.groovy
    Modified child device to be a momentary switch as well as a button, so that it can be seen in alexa, as the current alexa app does not support buttons.

--- devices/roku-app-child.groovy
+++ devices/roku-app-child.groovy
@@ -1,18 +1,34 @@
 /**
- *  Roku App
+ * Roku App
+ * This is a child device.  To use this device you simply need to install the device type handler.
+ * The parent device will take care of add instances of the device handler for each application that
+ * needs to be activated.  As this device is a generic push-button with a momentary switch, simillar to
+ * the scene controllers,  in a future version this will be modified to be a generic momentary switch child
+ * device instead.
  *
  */
 metadata {
 	definition (name: "Roku App", namespace: "apwelsh", author: "Armand Welsh") {
 		capability "Momentary"
-
+		capability "Switch"
+		capability "Actuator"
 	}
+}
 
+def on() {
+	sendEvent (name: "switch", value:"on")
+	def appId = device.deviceNetworkId.split("-")[1]
+	parent.launchApp(appId)
+	
+	runInMillis(3000, 'off')
+}
+
+def off() {
+	sendEvent (name: "switch", value:"off")	
 }
 
 def push() {
-	def appId = device.deviceNetworkId.split("-")[1]
-	parent.launchApp(appId)
+	on()
 }
 
 def parse(String description) {
