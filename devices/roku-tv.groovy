273ebdee922bddfdf53970e2c7eaa9de941c295c
Armand Welsh <armand@welshhome.org>
2019-06-06 10:16:44 -0700

    Refactor code for readability / add helpers
    Refactor code for readability, and create two new helper functions for computing networkId of child App device, and computer the appId from the child Device network Id.

--- devices/roku-tv.groovy
+++ devices/roku-tv.groovy
@@ -17,332 +17,362 @@
  *-------------------------------------------------------------------------------------------------------------------
  **/
 preferences {
-	input "deviceIp", "text", title: "Device IP", required: true
-	input "deviceMac", "text", title: "Device MAC Address", required: true, defaultValue: "UNKNOWN"
+    input "deviceIp", "text", title: "Device IP", required: true
+    input "deviceMac", "text", title: "Device MAC Address", required: true, defaultValue: "UNKNOWN"
 }
 
 metadata {
-	definition (name: "Roku TV", namespace: "apwelsh", author: "Armand Welsh") {
-		capability "TV"
-		capability "AudioVolume"
-		capability "Switch"
-		capability "Polling"
-		capability "Refresh"
+    definition (name: "Roku TV", namespace: "apwelsh", author: "Armand Welsh") {
+        capability "TV"
+        capability "AudioVolume"
+        capability "Switch"
+        capability "Polling"
+        capability "Refresh"
 
-		command "hdmi1"
-		command "hdmi2"
-		command "hdmi3"
-		command "hdmi4"
-		command "home"
-		
-		command "reloadApps"
+        command "hdmi1"
+        command "hdmi2"
+        command "hdmi3"
+        command "hdmi4"
+        command "home"
+        
+        command "reloadApps"
   }
 }
 
+/**
+ * Hubitat DTH Lifecycle Functions
+ **/
 def installed() {
-	updated()
+    updated()
 }
 
 def updated() {
-	log.debug "updated"
-	poll()
-	runEvery5Minutes(poll)
-	runEvery1Minute(queryCurrentApp)
+    log.debug "updated"
+    poll()
+    runEvery5Minutes(poll)
+    runEvery1Minute(queryCurrentApp)
 }
 
+/**
+ * Event Parsers
+ **/
 def parse(String description) {
-	def msg = parseLanMessage(description)
-	
-	if (msg.body) {
-		def body = new XmlParser().parseText(msg.body)
-		switch (body.name()) {
-			case "device-info":
-				parseMacAddress body
-				parsePowerState body
-				parseState body
-				break;
-			case "apps":
-				parseInstalledApps body
-				break;
-			case "active-app":
-				parseActiveApp body
-				break;
-		}
-	}
+    def msg = parseLanMessage(description)
+    
+    if (msg.body) {
+        def body = new XmlParser().parseText(msg.body)
+        switch (body.name()) {
+            case "device-info":
+                parseMacAddress body
+                parsePowerState body
+                parseState body
+                break;
+            case "apps":
+                parseInstalledApps body
+                break;
+            case "active-app":
+                parseActiveApp body
+                break;
+        }
+    }
 }
 
-def reloadApps() {
-	// parseInstalledApps new XmlParser().parseText("<app/>")
-	refresh()
+def networkIdForApp(String appId) {
+    return "${device.deviceNetworkId}-${appId}"    
+}
+
+def appIdForNetworkId(String netId) {
+    ​def group = (netId =~ /^(.*)\-([^-]*)$/)
+    if (group.size() == 1)
+        return "${group[0][2]}"
+    return netId
 }
 
 def parseInstalledApps(Node body) {
-	
-	childDevices.each{ child ->
-		//log.debug "child: ${child.deviceNetworkId} (${child.name})"
-		def nodeExists = false
-		body.app.each{ node ->
-			if ("${device.deviceNetworkId}-${node.attributes().id}" == child.deviceNetworkId)
-			nodeExists = true
-		}
-		if (!nodeExists) {
-			log.trace "Deleting child device: ${child.name} (${child.deviceNetworkId})"
-			deleteChildDevice(child.deviceNetworkId)
-		}
-	}
-	
-	body.app.each{ node ->
-		if (node.attributes().type != "appl") {
-			return
-		}
-		def appId = node.attributes().id
-		def appName = node.value()[0]
-		
-		//log.debug "Detected installed channel app: ${appName} (${device.deviceNetworkId}-${appId})"
-		
-		updateChild(appId, appName)
-	}
+    
+    childDevices.each{ child ->
+        //log.debug "child: ${child.deviceNetworkId} (${child.name})"
+        def nodeExists = false
+        body.app.each{ node ->
+            def netId = networkIdForApp(node.attributes().id)
+            if (netId == child.deviceNetworkId)
+                nodeExists = true
+        }
+        if (!nodeExists) {
+            log.trace "Deleting child device: ${child.name} (${child.deviceNetworkId})"
+            deleteChildDevice(child.deviceNetworkId)
+        }
+    }
+    
+    body.app.each{ node ->
+        if (node.attributes().type != "appl") {
+            return
+        }
+
+        def netId = networkIdForApp(node.attributes().id)
+        def appName = node.value()[0]
+        updateChildApp(netId, appName)
+    }
 }
 
 def parseActiveApp(Node body) {
-	def app = body.app[0]?.value()
-	if (app != null) {
-		def currentApp = app[0]
-		if (currentApp != this.state."active-app") {
-			log.debug "set active-app = ${currentApp}"
-			this.state."active-app" = currentApp
-		}
-	}
+    def app = body.app[0]?.value()
+    if (app != null) {
+        def currentApp = app[0]
+        if (currentApp != this.state."active-app") {
+            log.debug "set active-app = ${currentApp}"
+            this.state."active-app" = currentApp
+        }
+    }
 }
 
 def parseState(Node body) {
-	for (def node : body) {
-		def key = node.name()
-		if (key == null)
-			continue
-		
-		switch (key) {
-			case "serial-number":
-			case "vendor-name":
-			case "device-id":
-			case "mode-name":
-			case "screen-size":
-			case "user-device-name":
-				def value = node.value()
-				if (value != null) {
-					if (value[0] != this.state."${key}") {
-						this.state."${key}" = value[0]
-						log.debug "set ${key} = ${value[0]}"
-					}
-				}
-		}
-	}
+    for (def node : body) {
+        def key = node.name()
+        if (key == null)
+            continue
+        
+        switch (key) {
+            case "serial-number":
+            case "vendor-name":
+            case "device-id":
+            case "mode-name":
+            case "screen-size":
+            case "user-device-name":
+                def value = node.value()
+                if (value != null) {
+                    if (value[0] != this.state."${key}") {
+                        this.state."${key}" = value[0]
+                        log.debug "set ${key} = ${value[0]}"
+                    }
+                }
+        }
+    }
 }
 
 def parseMacAddress(Node body) {
-	def wifiMac = body."wifi-mac"[0]?.value()
-	if (wifiMac != null) {
-		def macAddress = wifiMac[0].replaceAll("[^A-f,a-f,0-9]","")
-		if (!deviceMac || deviceMac != macAddress) {
-			log.debug "Update config [MAC Address = ${macAddress}]"
-			device.updateSetting("deviceMac", [value: macAddress, type:"text"])
-		}
-	}
+    def wifiMac = body."wifi-mac"[0]?.value()
+    if (wifiMac != null) {
+        def macAddress = wifiMac[0].replaceAll("[^A-f,a-f,0-9]","")
+        if (!deviceMac || deviceMac != macAddress) {
+            log.debug "Update config [MAC Address = ${macAddress}]"
+            device.updateSetting("deviceMac", [value: macAddress, type:"text"])
+        }
+    }
 }
 
 def parsePowerState(Node body) {
-	def powerMode = body."power-mode"[0]?.value()
-	if (powerMode != null) {
-		def mode = powerMode[0]
-		switch (mode) {
-			case "PowerOn":
-				if (this.state!="on") {
-					sendEvent(name: "switch", value: "on")
-				} 
-				break;
-			case "PowerOff":
-			case "DisplayOff":
-			case "Headless":
-				if (this.state!="off") {
-					sendEvent(name: "switch", value: "off")
-				}
-				break;
-		}
-	}	
+    def powerMode = body."power-mode"[0]?.value()
+    if (powerMode != null) {
+        def mode = powerMode[0]
+        switch (mode) {
+            case "PowerOn":
+                if (this.state!="on") {
+                    sendEvent(name: "switch", value: "on")
+                } 
+                break;
+            case "PowerOff":
+            case "DisplayOff":
+            case "Headless":
+                if (this.state!="off") {
+                    sendEvent(name: "switch", value: "off")
+                }
+                break;
+        }
+    }    
 }
 
+/*
+ * Device Capability Interface Functions
+ */
+
 def on() {
-	sendEvent(name: "switch", value: "on")
+    sendEvent(name: "switch", value: "on")
 
-	sendHubCommand(new hubitat.device.HubAction (
-		"wake on lan ${deviceMac}",
-		hubitat.device.Protocol.LAN,
-		null,
-		[:]
-	))
+    sendHubCommand(new hubitat.device.HubAction (
+        "wake on lan ${deviceMac}",
+        hubitat.device.Protocol.LAN,
+        null,
+        [:]
+    ))
 
-	keypress('Power')
+    keypress('Power')
 }
 
 def off() {
-	sendEvent(name: "switch", value: "off")
-	keypress('PowerOff')
+    sendEvent(name: "switch", value: "off")
+    keypress('PowerOff')
 }
 
 def home() {
-	keypress('Home')
+    keypress('Home')
 }
 
 def channelUp() {
-	keypress('ChannelUp')
+    keypress('ChannelUp')
 }
 
 def channelDown() {
-	keypress('ChannelDown')
+    keypress('ChannelDown')
 }
 
 def volumeUp() {
-	keypress('VolumeUp')
+    keypress('VolumeUp')
 }
 
 def volumeDown() {
-	keypress('VolumeDown')
+    keypress('VolumeDown')
 }
 
 def setVolume() {
-	
+    
 }
 
 def unmute() {
-	keypress('VolumeMute')
+    keypress('VolumeMute')
 }
 
 def mute() {
-	keypress('VolumeMute')
-}
-
-def hdmi1() {
-	keypress('InputHDMI1')
-}
-
-def hdmi2() {
-	keypress('InputHDMI2')
-}
-
-def hdmi3() {
-	keypress('InputHDMI3')
-}
-
-def hdmi4() {
-	keypress('InputHDMI4')
+    keypress('VolumeMute')
 }
 
 def poll() {
-	log.debug "Executing 'poll'"
-	refresh()
+    log.debug "Executing 'poll'"
+    refresh()
 }
 
 def refresh() {
-	log.debug "Executing 'refresh'"
-	queryDeviceState()
-	queryInstalledApps()
-	//queryCurrentApp()
+    log.debug "Executing 'refresh'"
+    queryDeviceState()
+    queryInstalledApps()
 }
 
-def queryDeviceState() {
+/**
+ * Custom DTH Command interface functions
+ **/
 
-	sendHubCommand(new hubitat.device.HubAction(
-		method: "GET",
-		path: "/query/device-info",
-		headers: [ HOST: "${deviceIp}:8060" ]
-	))
+def hdmi1() {
+    keypress('InputHDMI1')
+}
+
+def hdmi2() {
+    keypress('InputHDMI2')
+}
+
+def hdmi3() {
+    keypress('InputHDMI3')
+}
+
+def hdmi4() {
+    keypress('InputHDMI4')
+}
+
+def reloadApps() {
+    // parseInstalledApps new XmlParser().parseText("<app/>")
+    refresh()
+}
+
+/**
+ * Roku API Section
+ * The following functions are used to communicate with the Roku RESTful API
+ **/
+
+def queryDeviceState() {
+    sendHubCommand(new hubitat.device.HubAction(
+        method: "GET",
+        path: "/query/device-info",
+        headers: [ HOST: "${deviceIp}:8060" ]
+    ))
 }
 
 def queryCurrentApp() {
-	sendHubCommand(new hubitat.device.HubAction(
-		method: "GET",
-		path: "/query/active-app",
-		headers: [ HOST: "${deviceIp}:8060" ]
-	))
-	
+    sendHubCommand(new hubitat.device.HubAction(
+        method: "GET",
+        path: "/query/active-app",
+        headers: [ HOST: "${deviceIp}:8060" ]
+    ))
+    
 }
 
 def queryInstalledApps() {
-	sendHubCommand(new hubitat.device.HubAction(
-		method: "GET",
-		path: "/query/apps",
-		headers: [ HOST: "${deviceIp}:8060" ]
-	))
+    sendHubCommand(new hubitat.device.HubAction(
+        method: "GET",
+        path: "/query/apps",
+        headers: [ HOST: "${deviceIp}:8060" ]
+    ))
 }
 
 def keypress(key) {
-	log.debug "Executing '${key}'"
-	def result = new hubitat.device.HubAction(
-		method: "POST",
-		path: "/keypress/${key}",
-		headers: [ HOST: "${deviceIp}:8060" ],
-		body: "",
+    log.debug "Executing '${key}'"
+    def result = new hubitat.device.HubAction(
+        method: "POST",
+        path: "/keypress/${key}",
+        headers: [ HOST: "${deviceIp}:8060" ],
+        body: "",
 
-	)
+    )
 }
 
 def launchApp(appId) {
-	log.debug "Executing 'launchApp ${appId}'"
-	def result = new hubitat.device.HubAction(
-		method: "POST",
-		path: "/launch/${appId}",
-		headers: [ HOST: "${deviceIp}:8060" ],
-		body: "",
-	)
-	sendHubCommand(result)
-	runInMillis(3000, 'queryCurrentApp')
-	runInMillis(5000, 'queryCurrentApp')
-	runInMillis(10000, 'queryCurrentApp')
+    log.debug "Executing 'launchApp ${appId}'"
+    def result = new hubitat.device.HubAction(
+        method: "POST",
+        path: "/launch/${appId}",
+        headers: [ HOST: "${deviceIp}:8060" ],
+        body: "",
+    )
+    sendHubCommand(result)
+
+    // Because each Roku behaves differently, with different timings,
+    // and since the launchApp function is asynchronous
+    // we will schedule a few checks for the current app
+    // This will help for future support of changing App devices to on/off switches.
+    runInMillis(3000, 'queryCurrentApp')
+    runInMillis(5000, 'queryCurrentApp')
+    runInMillis(10000, 'queryCurrentApp')
 }
 
-def updateChild(String appId, String appName) {
-    
-	//Try to find existing child device
-    def child = getChild(appId)
+/**
+ * Child Device Maintenance Section
+ * These functions are used to manage the child devices bound to this device
+ */
 
-    //If child exists, do not create it
-	if(child != null) {
-		return
-	}
-
-	if (appName != null) {
-		if (logEnable) log.debug "child with appId = ${appId}, appName = ${appName}  does not exist."
-		createChildDevice(appId, appName)
-		child = getChild(appId)
-	} else {
-		log.error "Cannot create child device for ${appId} due to missing 'appName'"
-	}
-}
-
-private def getChild(String appId) {
-	
-	def netId = "${device.deviceNetworkId}-${appId}"
+private def getChildDevice(String netId) {
     try {
-		def result = null
+    def result = null
         childDevices.each{ child ->
             if(child.deviceNetworkId == netId) {
                 result = child
             }
         }
-		return result
+        return result
     } catch(e) {
         log.error "Failed to find child with exception: ${e}";
     }
-	return null
+    return null
 }
 
-private void createChildDevice(String appId, String appName) {
+def updateChildApp(String netId, String appName) {
+    def child = getChildDevice(netId)
+    if(child != null) {
+        //If child exists, do not create it
+        return
+    }
+
+    if (appName != null) {
+        //log.debug "Child does not exist: ${appName} (${netId})"
+        createChildApp(netId, appName)
+    } else {
+        log.error "Cannot create child: (${netId}) due to missing 'appName'"
+    }
+}
+
+private void createChildApp(String netId, String appName) {
     try {
-		def netId = "${device.deviceNetworkId}-${appId}"
         addChildDevice("Roku App", "${netId}",
-            [label: "${device.displayName}-${appName}", 
+            [label: "${netId}", 
              isComponent: false, name: "${appName}"])
-		log.trace "Created child device: ${appName} (${netId})"
+        log.trace "Created child device: ${appName} (${netId})"
     } catch(e) {
         log.error "Failed to create child device with exception: ${e}"
     }
