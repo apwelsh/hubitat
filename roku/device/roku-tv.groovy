a87889d06acc197ed0a7901da6bcb144f1b78997
Armand Welsh <armand@welshhome.org>
2021-01-18 13:50:06 -0800

    Update version

--- roku/device/roku-tv.groovy
+++ roku/device/roku-tv.groovy
@@ -1,6 +1,6 @@
 /**
  * Roku TV
- * Version 2.5.1
+ * Version 2.5.2
  * Download: https://github.com/apwelsh/hubitat
  * Description:
  * This is a parent device handler designed to manage and control a Roku TV or Player connected to the same network 
